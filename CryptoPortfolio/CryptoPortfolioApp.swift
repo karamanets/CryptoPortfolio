701727b610f29696aae42a0e457aebc758dcfa58
Alex Karamanets <alexander.fromworld@gmail.com>
2023-04-17 16:57:26 +0200

    Start crypto portfolio app

---
+++ CryptoPortfolio/CryptoPortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoPortfolioApp.swift
+//  CryptoPortfolio
+//
+//  Created by Alex Karamanets on 17.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct CryptoPortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
