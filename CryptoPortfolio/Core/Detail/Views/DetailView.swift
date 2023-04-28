b37757ee889eabd0cede11a5efe90fc4b2e019eb
Alex Karamanets <alexander.fromworld@gmail.com>
2023-04-28 16:05:50 +0200

    Added DetailsViewModel and subscriber, make detailView statistic

--- CryptoPortfolio/Core/Detail/Views/DetailView.swift
+++ CryptoPortfolio/Core/Detail/Views/DetailView.swift
@@ -11,29 +11,89 @@ struct DetailView: View {
     
     @StateObject private var vm: DetailViewModel
     
+    private let columns: [GridItem] = [GridItem(.flexible()), GridItem(.flexible())]
+    
     /// inject CoinModel
     init(coin: CoinModel) {
         _vm = StateObject(wrappedValue: DetailViewModel(coin: coin))
-        //print("[🔥] Coin downloaded: \(coin.name)")
     }
     
     var body: some View {
         VStack {
-           
-            XMarkButton()
-            
-        }
+            detailHeader
+                ScrollView(showsIndicators: false) {
+                    graph
+
+                    Divider()
+                    overview
+
+                    details
+                }
+                .padding()
             .navigationBarBackButtonHidden()
+        }
     }
 }
 
 //                🔱
 struct DetailView_Previews: PreviewProvider {
     static var previews: some View {
-        DetailView(coin: dev.coin)
+            DetailView(coin: dev.coin)
     }
 }
 
-
-
+//MARK: - Components
+extension DetailView {
+    
+    /// Header coin name and backButton
+    private var detailHeader: some View {
+        HStack  {
+            XMarkButton()
+            Text(vm.coin.name)
+                .font(.title2 .bold())
+                .foregroundColor(Color.theme.accent)
+            Spacer()
+        }
+    }
+    
+    /// Graph for coin
+    private var graph: some View {
+        VStack {
+            Text("Grafic")
+                .frame(height: 150)
+        }
+    }
+    
+    /// Overview for coin
+    private var overview: some View {
+        VStack (spacing: 20) {
+            Text("Overview")
+                .font(.title .bold())
+                .foregroundColor(Color.theme.accent)
+                .frame(maxWidth: .infinity, alignment: .leading)
+            
+            LazyVGrid(columns: columns, alignment: .leading, spacing: 30) {
+                ForEach(vm.overviewStatistic) { stat in
+                    StatisticBarView(statistic: stat)
+                }
+            }
+        }
+    }
+    
+    /// Details for coin
+    private var details: some View {
+        VStack (spacing: 20) {
+            Text("Details")
+                .font(.title .bold())
+                .foregroundColor(Color.theme.accent)
+                .frame(maxWidth: .infinity, alignment: .leading)
+            
+            LazyVGrid(columns: columns, alignment: .leading, spacing: 30) {
+                ForEach(vm.detailsStatistic) { stat in
+                    StatisticBarView(statistic: stat)
+                }
+            }
+        }
+    }
+}
 
